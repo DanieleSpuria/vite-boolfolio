9102181dc12b6f399c2cbf23dacf3c250bc471fc
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-28 17:54:13 +0200

    add getProject

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,6 +1,23 @@
 <script>
+  import axios from 'axios';
+  import {store} from '../store/store';
+
   export default {
-  name: 'Project'    
+    name: 'Project',
+    project: null,
+    
+    methods: {
+      getProject(endpoint) {
+        axios.get(endpoint)
+          .then(result => {
+            this.project = result.data;
+          })
+      }
+    },
+
+    mounted() {
+      this.getProject(store.url + 'projects/' + this.$route.params.slug);
+    }
   }
 </script>
 
