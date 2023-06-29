1b8a06c1b4b2641d6ba101bd97440444f4aa2eb6
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-29 15:04:21 +0200

    add ContactForm

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,43 @@
+<script>
+  export default {
+    name: 'ContactForm',
+    
+    data() {
+      return {
+        name: '',
+        email: '',
+        text: ''
+      }
+    },
+
+    methods: {
+      sendForm() {
+        const data = {
+          name: this.name,
+          email: this.email,
+          text: this.text 
+        }
+
+        console.log(data);
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="box-email">
+    <input v-model="this.name" type="text" placeholder="Insert your name">
+    <input v-model="this.email" type="email" placeholder="Insert email">
+    <textarea v-model="this.text" cols="30" rows="10" placeholder="Insert text"></textarea>
+    <button @click="sendForm()">Submit</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  input,
+  textarea {
+    display: block;
+    margin: 20px 0;
+    width: 100%;
+  }
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,11 +1,18 @@
 <script>
+  import ContactForm from '../components/ContactForm.vue';
+
   export default {
-    name: 'contacts'  
+    name: 'contacts',
+    
+    components: {
+      ContactForm
+    }
   }
 </script>
 
 <template>
   <h1>Contacts</h1>
+  <ContactForm/>
 </template>
 
 <style>
