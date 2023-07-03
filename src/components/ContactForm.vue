b084066fca386203d0d574e7296bde847fdd30a7
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-07-04 00:02:03 +0200

    print error

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -9,7 +9,9 @@
       return {
         name: '',
         email: '',
-        text: ''
+        text: '',
+        errors: {},
+        success: false
       }
     },
 
@@ -22,7 +24,13 @@
         }
         axios.post(store.url + 'contacts/', data)
           .then(result => {
-            console.log(result);
+            this.success = false;
+            if (result.data.errors) {
+              this.errors = result.data.errors;
+            } else {
+              this.errors =  {};
+              this.success = true;
+            }
           })
       }
     }
@@ -30,10 +38,21 @@
 </script>
 
 <template>
-  <div class="box-email">
+  <span v-if="success">Email inviata correttamente!</span>
+
+  <div v-else class="box-email">
+    <label for="name">Name</label>
     <input v-model="this.name" type="text" placeholder="Insert your name">
+    <span v-for="(error, index) in errors.name" :key="index">{{ error }}</span>
+
+    <label for="email">Email</label>
     <input v-model="this.email" type="email" placeholder="Insert email">
+    <span v-for="(error, index) in errors.email" :key="index">{{ error }}</span>
+
+    <label for="text">Text</label>
     <textarea v-model="this.text" cols="30" rows="10" placeholder="Insert text"></textarea>
+    <span v-for="(error, index) in errors.text" :key="index">{{ error }}</span>
+
     <button @click="sendForm()">Submit</button>
   </div>
 </template>
@@ -42,7 +61,11 @@
   input,
   textarea {
     display: block;
-    margin: 20px 0;
+    margin-bottom: 10px;
     width: 100%;
+  }
+
+  span {
+    display: block;
   }
 </style>
